1d892b323546d95f64761830554a4f2ebe41ad3f
tonyminh95 <minhbui12121995@gmail.com>
2020-10-09 12:36:01 +0900

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-    <renderless-tags-input>
-        <div slot-scope="{}" class="tags-input">
-        <span class="tags-input-tag">
-            <span>Testing</span>
-            <span>Design</span>
-            <button type="button" class="tags-input-remove">&times;</button>
-        </span>
-        <input class="tags-input-text" placeholder="Add tag...">
+    <renderless-tags-input v-model='tags'>
+        <div slot-scope='{ tags, removeTag, inputAttrs, inputEvents }' class='tags-input'>
+            <span class='tags-input-tag'>
+                <span v-for='(tag, index) in tags' :key='index'>
+                    {{ tag }}
+
+                    <button type='button' class='tags-input-remove'
+                        @click='removeTag(tag)'
+                    >
+                        &times;
+                    </button>
+                </span>
+            </span>
+
+            <input class="tags-input-text" placeholder="Add tag..."
+                v-bind="inputAttrs"
+                v-on="inputEvents"
+            >
         </div>
     </renderless-tags-input>
 </template>
--- src/components/RenderlessTagsInput.vue
+++ src/components/RenderlessTagsInput.vue
@@ -4,9 +4,44 @@
 
         props: ['value'],
 
+        data () {
+            return {
+                newTag: ''
+            }
+        },
+
+        methods: {
+            addTag() {
+                if (this.newTag.trim().length === 0 || this.value.includes(this.newTag.trim())) {
+                    return
+                }
+
+                this.$emit('input', [...this.value, this.newTag.trim()])
+                this.newTag = ''
+            },
+
+            removeTag(tag) {
+                this.$emit('input', this.value.filter(t => t !== tag))
+            }
+        },
+
         render() {
             return this.$scopedSlots.default({
-                tags: this.value
+                tags: this.value,
+                removeTag: this.removeTag,
+                inputAttrs: {
+                    value: this.newTag,
+                },
+                inputEvents: {
+                    input: (e) => { this.newTag = e.target.value },
+
+                    keydown: (e) => {
+                        if (e.keyCode === 13) {
+                            e.preventDefault()
+                            this.addTag()
+                        }
+                    }
+                }
             })
         }
     }
